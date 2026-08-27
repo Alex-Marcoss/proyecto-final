e05d1ea9803cda41db85820a072e82318c82e2e4
Facundo735 <Facundo.sanzone@gmail.com>
2026-08-27 16:23:45 -0300

    Update generadorEnemigos.java

--- Clases/enemigos/generadorEnemigos.java
+++ Clases/enemigos/generadorEnemigos.java
@@ -4,18 +4,18 @@ public class GeneradorEnemigos {
 
     public Enemigo generar(CicloDia ciclo) {
 
-        if (!ciclo.esNoche()) {
+        if (!ciclo.esNoche()) { // si durante el ciclo dia/noche es de dia los enemigos no apareceran
             System.out.println("Es de día. No aparecen enemigos.");
             return null;
         }
 
         int numero = (int)(Math.random() * 2);
-
-        if (numero == 0) {
+        
+        if (numero == 0) { // el enemigo varia dependiendo el math.random que elige un numero aleatorio
             return new Lobo();
         } 
         else {
-            return new Araña();
+            return new Araña(); // el enemigo varia dependiendo el math.random que elige un numero aleatorio
         }
     }
 }

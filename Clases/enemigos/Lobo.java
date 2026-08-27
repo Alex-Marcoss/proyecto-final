d6a06230e3f606b3aa7bb37c1b1676b8dea95d97
Facundo735 <Facundo.sanzone@gmail.com>
2026-08-27 16:22:36 -0300

    Update Lobo.java

--- Clases/enemigos/Lobo.java
+++ Clases/enemigos/Lobo.java
@@ -1,8 +1,8 @@
 package Juego;
 
-public class Lobo extends Enemigo {
+public class Lobo extends Enemigo { // clase hija de enemigo, Criatura definida
 
-	public Lobo(String nombre, int vida, int daño) {
+	public Lobo(String nombre, int vida, int daño) { // constructor
 		
 		super("Lobo",50,10);
 		

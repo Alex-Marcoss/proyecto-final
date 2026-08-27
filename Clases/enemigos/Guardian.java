e6c99ac6b4cbbb3980059a5d2336dd6597632f5f
Facundo735 <Facundo.sanzone@gmail.com>
2026-08-27 16:22:22 -0300

    Update Guardian.java

--- Clases/enemigos/Guardian.java
+++ Clases/enemigos/Guardian.java
@@ -1,8 +1,8 @@
 package Juego;
 
-public class Guardian extends Enemigo {
+public class Guardian extends Enemigo { // clase hija de enemigo, Criatura definida
 
-	public Guardian(String nombre, int vida, int daño) {
+	public Guardian(String nombre, int vida, int daño) { // constructor
 		
 		super("Guardian del Templo",250,35);
 		

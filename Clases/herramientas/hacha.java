49ea0df94be7a8975a795331409406f5eaad7d5e
Facundo735 <Facundo.sanzone@gmail.com>
2026-08-27 15:30:40 -0300

    Update hacha.java

--- Clases/herramientas/hacha.java
+++ Clases/herramientas/hacha.java
@@ -3,7 +3,7 @@ package juego;
 
 public class Hacha extends Herramienta {
 
-    public Hacha(int durabilidad, int daño) {
-        super("Hacha", durabilidad, daño, tipoHerramienta.HACHA);
+    public Hacha(int durabilidad, int daño) { // constructor
+        super("Hacha", durabilidad, daño, tipoHerramienta.HACHA); //definicion de hacha como herramienta
     }
 }	

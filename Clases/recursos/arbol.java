178f8f105ae64bdf78c2b1425a9fafa582dfed99
Facundo735 <Facundo.sanzone@gmail.com>
2026-08-27 15:25:09 -0300

    Enhance arbol class with comments for clarity
    Added comments to clarify the constructor and methods.

--- Clases/recursos/arbol.java
+++ Clases/recursos/arbol.java
@@ -4,18 +4,22 @@ public class arbol extends objetoMundo {
 
     private int maderaDisponible;
 
-    public arbol(int maderaDisponible) {
+    public arbol(int maderaDisponible) { // Constructor
         super("Arbol");
         this.maderaDisponible = maderaDisponible;
     }
 
-    @Override
-    public tipoHerramienta getHerramientaNecesaria() {
-        return tipoHerramienta.HACHA;
-    }
+    // -------------------------------------------------------------
 
     @Override
-    public Recursos recolectarRecurso() {
+    public tipoHerramienta getHerramientaNecesaria() {
+        return tipoHerramienta.HACHA; // el arbol recibe que solo puede ser talado con el tipo de herramienta HACHA
+    }
+
+    // ----------------------------------------------------------------
+    
+    @Override
+    public Recursos recolectarRecurso() { // funcion para recolectar recurso madera
 
         if (maderaDisponible <= 0) {
             return null;
@@ -28,8 +32,10 @@ public class arbol extends objetoMundo {
         return new Recursos("Madera", cantidad);
     }
 
+    // ----------------------------------------------------------------
+    
     @Override
     public int getEnergiaNecesaria() {
-        return 3;
+        return 3; // gasta la energia necesaria para talar
     }
 }
